3a1685deebd4e31cca2c06cad59c18309fc7b505
Anna <anya_aki@mail.ru>
2021-01-28 16:40:55 +0300

    мобильная версия

---
+++ source/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $(".slider").slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+});
+
+
+
+// $(document).ready(function(){
+//   $('.acord-content').hide();
+//   $('.text-block__info').click(function(){
+//       $(this).next().toggle()});
+// });

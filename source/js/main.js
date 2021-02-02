c87fc39cafb09b2f65e0e2f2453bc6b63d91ff24
Anna <anya_aki@mail.ru>
2021-02-02 16:34:57 +0300

    окончательно адаптивный слайдер

--- source/js/main.js
+++ source/js/main.js
@@ -1,18 +1,36 @@
-$(document).ready(function(){
-  $(".slider").slick({
-    arrows: true,
-    dots: true,
-    adaptiveHeight: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: "0",
+function mobileOnlySlider() {
+  $(document).ready(function(){
+      $('.slider').slick({
+        arrows: true,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: "0",
+        responsive: [{
+          breakpoint: 1300,
+          settings: {
+            slidesToShow: 1,
+            settings:"unslick"
+          }
+        }]
+    });
   });
-});
+}
+if (window.innerWidth < 1300) {
+  mobileOnlySlider();
+}
 
-window.addEventListener("resize", function () {
-  var withWindow = window.innerWidth;
-  if (withWindow > 1300) {
-    $('.slider').slick('unslick');
+$(window).resize(function(e){
+  if (window.innerWidth <= 1300) {
+    if (!$('.slider').hasClass('slick-initialized')){
+      mobileOnlySlider();
+    }
+
+  } else {
+      if($('.slider').hasClass('slick-initialized')){
+        $('.slider').slick('unslick');
+    }
   }
 });
